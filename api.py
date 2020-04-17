21fde518738cd5b23dced1e4786c7fc86e94d939
Rito Tanaka <ritotanaka@Macintosh.local>
2020-04-17 00:32:25 -0400

    works successfully + added tax rates

--- api.py
+++ api.py
@@ -13,8 +13,10 @@ class MakeLeapsAPI:
         self.client_secret = client_secret
         self.token = None
 
+    """ Authenticates Client """
+    # Returns access token
     def _auth_client(self):
-        url = 'https://api.makeleaps.com/user/oauth2/token/'
+        url = 'https://api-meetup.makeleaps.com/user/oauth2/token/'
 
         creds = f'{self.client_id}:{self.client_secret}'.encode('utf-8')
         creds_encoded = base64.b64encode(creds).decode('utf-8')
@@ -25,11 +27,11 @@ class MakeLeapsAPI:
         response = requests.post(url, data=data, headers=headers)
         response_json = response.json()
 
-        return response_json
+        return response_json['access_token']
 
     """ Make authenticated POST requests to API """
     # The API will check if Bearer token in request is valid,
-    # and is so authenticate the request
+    # and if so authenticate the request
     def post(self, token, url, data):
         headers = {
             'Content-Type': 'application/json',
@@ -41,9 +43,22 @@ class MakeLeapsAPI:
         return (response.status_code, response.json()['response'])
 
     """ Make authenticated GET requests to API """
-    def get(self, url):
-        headers = {'Content-Type': 'application/json'}
-        headers = self._authorize_header(headers=headers)
+    def get(self, token, url):
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {token}',
+        }
         response = requests.get(url, headers=headers)
+
+        return (response.status_code, response.json()['response'])
+
+    # TODO: Figure out correct header format
+    """ Make authenticated PUT request for uploading PDF """
+    def put(self, token, url):
+        headers = {
+            'Content-Type': 'application/pdf',
+            'Authorization': f'Bearer {token}',
+        }
+        response = requests.put(url, headers=headers)
 
         return (response.status_code, response.json()['response'])
